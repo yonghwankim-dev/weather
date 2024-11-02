2a82a3050bd4dcb505b7f688a1de2b3a95f91108
yonghwankim-dev <dragonbead95@naver.com>
2024-11-02 13:44:35 +0900

    feat: 도시의 날씨 정보 가져오기 API 구현

--- src/main/java/site/weather/api/weather/service/WeatherService.java
+++ src/main/java/site/weather/api/weather/service/WeatherService.java
@@ -1,2 +1,31 @@
-package site.weather.api.weather.service;public class WeatherService {
+package site.weather.api.weather.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class WeatherService {
+
+	private final String appid;
+
+	public WeatherService(@Value("${weather.appid}") String appid) {
+		this.appid = appid;
+	}
+
+	public String fetchWeatherByCity(String city) {
+		WebClient webClient = WebClient.builder()
+			.baseUrl("https://api.openweathermap.org")
+			.build();
+		return webClient.get()
+			.uri(uriBuilder -> uriBuilder
+				.path("/data/2.5/weather")
+				.queryParam("q", city)
+				.queryParam("appid", appid)
+				.queryParam("units", "metric")
+				.build())
+			.retrieve()
+			.bodyToMono(String.class)
+			.block();
+	}
 }
