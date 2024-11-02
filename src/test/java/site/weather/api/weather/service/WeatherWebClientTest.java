fdf511ef73345b6686067905a0e3d8b86d848250
yonghwankim-dev <dragonbead95@naver.com>
2024-11-02 15:26:46 +0900

    test: MockWebServer 추가

--- src/test/java/site/weather/api/weather/service/WeatherWebClientTest.java
+++ src/test/java/site/weather/api/weather/service/WeatherWebClientTest.java
@@ -1,25 +1,57 @@
 package site.weather.api.weather.service;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class WeatherWebClientTest {
 
-	@Autowired
+	private MockWebServer mockWebServer;
 	private WeatherWebClient client;
+
+	@Value("${weather.appid}")
+	private String appid;
+
+	@BeforeEach
+	void setup() {
+		this.mockWebServer = new MockWebServer();
+		try {
+			mockWebServer.start();
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+		this.client = new WeatherWebClient(WebClient.builder()
+			.baseUrl(mockWebServer.url("/").toString())
+			.build(), appid);
+	}
 
 	@DisplayName("서울의 날씨 정보를 조회한다")
 	@Test
 	void givenCityName_whenFetchWeatherByCity_thenReturnWeatherOfSeoul() {
 		// given
+		MockResponse mockResponse = new MockResponse()
+			.setResponseCode(200)
+			.setBody("ok");
+		mockWebServer.enqueue(mockResponse);
+
 		String city = "Seoul";
 		// when
 		String json = client.fetchWeatherByCity(city);
 		// then
-		Assertions.assertThat(json).isNotNull();
+		assertThat(json).isEqualTo("ok");
 	}
 }
