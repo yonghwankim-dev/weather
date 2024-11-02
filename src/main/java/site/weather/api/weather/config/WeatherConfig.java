c31e17e51643e3bc85c0c265ebd4f33c75d2b63d
yonghwankim-dev <dragonbead95@naver.com>
2024-11-02 16:53:53 +0900

    feat: WebClient에 jackson 설정 추가

--- src/main/java/site/weather/api/weather/config/WeatherConfig.java
+++ src/main/java/site/weather/api/weather/config/WeatherConfig.java
@@ -3,12 +3,20 @@ package site.weather.api.weather.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
 import site.weather.api.weather.service.WeatherWebClient;
 
 @Configuration
+@RequiredArgsConstructor
 public class WeatherConfig {
+
+	private final ObjectMapper objectMapper;
 
 	@Bean
 	public WeatherWebClient weatherWebClient(
@@ -16,6 +24,10 @@ public class WeatherConfig {
 		@Value("${weather.appid}") String appid) {
 		WebClient webClient = WebClient.builder()
 			.baseUrl(baseUrl)
+			.codecs(configurer -> {
+				configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
+				configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
+			})
 			.build();
 		return new WeatherWebClient(webClient, appid);
 	}
