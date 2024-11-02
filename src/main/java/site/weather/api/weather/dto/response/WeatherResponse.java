57e3ceba69634b8c7fe1a25baab1b8f0aee0984c
yonghwankim-dev <dragonbead95@naver.com>
2024-11-02 16:49:09 +0900

    feat: WeatherResponse 클래스의 역직렬화 설정 추가

--- src/main/java/site/weather/api/weather/dto/response/WeatherResponse.java
+++ src/main/java/site/weather/api/weather/dto/response/WeatherResponse.java
@@ -1,6 +1,5 @@
 package site.weather.api.weather.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.EqualsAndHashCode;
@@ -9,14 +8,16 @@ import lombok.EqualsAndHashCode;
 public class WeatherResponse {
 	@JsonProperty("name")
 	private final String name;
-
-	@JsonCreator
-	public WeatherResponse(@JsonProperty("name") String name) {
+	@JsonProperty("temperature")
+	private final double temperature;
+	
+	public WeatherResponse(String name, double temperature) {
 		this.name = name;
+		this.temperature = temperature;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("(name=%s", name);
+		return String.format("(name=%s, temperature=%.2f", name, temperature);
 	}
 }
