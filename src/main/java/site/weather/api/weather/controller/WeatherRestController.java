8a41c7eeefa7c5c7216856c8b20add139e8ec8bb
yonghwankim-dev <dragonbead95@naver.com>
2024-11-02 13:44:35 +0900

    feat: 도시의 날씨 정보 가져오기 API 구현

--- src/main/java/site/weather/api/weather/controller/WeatherRestController.java
+++ src/main/java/site/weather/api/weather/controller/WeatherRestController.java
@@ -3,14 +3,22 @@ package site.weather.api.weather.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.weather.api.weather.service.WeatherService;
 
 @RestController
 @RequestMapping("/api/weather")
+@RequiredArgsConstructor
 public class WeatherRestController {
 
+	private final WeatherService service;
+
 	@GetMapping
-	public ResponseEntity<String> getWeather() {
-		return ResponseEntity.ok("weather");
+	public ResponseEntity<String> getWeather(@RequestParam String city) {
+		String json = service.fetchWeatherByCity(city);
+		return ResponseEntity.ok(json);
 	}
 }
