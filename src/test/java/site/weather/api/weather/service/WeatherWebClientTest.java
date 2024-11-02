00f7e3db1755c47798801586a60765aef7c5c88c
yonghwankim-dev <dragonbead95@naver.com>
2024-11-02 16:17:58 +0900

    test: json 데이터를 응답하도록 수정

--- src/test/java/site/weather/api/weather/service/WeatherWebClientTest.java
+++ src/test/java/site/weather/api/weather/service/WeatherWebClientTest.java
@@ -1,18 +1,25 @@
 package site.weather.api.weather.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import site.weather.api.weather.dto.response.WeatherResponse;
 
 @SpringBootTest
 class WeatherWebClientTest {
@@ -22,6 +29,16 @@ class WeatherWebClientTest {
 
 	@Value("${weather.appid}")
 	private String appid;
+
+	private static String getWeatherJson(String path) {
+		ClassPathResource resource = new ClassPathResource(path);
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+			return br.lines()
+				.collect(Collectors.joining());
+		} catch (IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
 
 	@BeforeEach
 	void setup() {
@@ -41,16 +58,18 @@ class WeatherWebClientTest {
 	void givenCityName_whenFetchWeatherByCity_thenReturnWeatherOfSeoul() {
 		// given
 		MockResponse mockResponse = new MockResponse()
+			.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
 			.setResponseCode(200)
-			.setBody("ok");
+			.setBody(getWeatherJson("weather.json"));
 		mockWebServer.enqueue(mockResponse);
 
 		String city = "Seoul";
 		// when
-		Mono<String> source = client.fetchWeatherByCity(city);
+		Mono<WeatherResponse> source = client.fetchWeatherByCity(city);
 		// then
+		WeatherResponse expected = new WeatherResponse("Seoul");
 		StepVerifier.create(source)
-			.expectNext("ok")
+			.expectNext(expected)
 			.verifyComplete();
 	}
 }
