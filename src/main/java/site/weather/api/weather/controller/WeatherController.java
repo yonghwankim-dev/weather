2b0ac23a4bcbbd6aa796933876288d047e57460b
yonghwankim-dev <dragonbead95@naver.com>
2024-11-04 13:21:50 +0900

    refactor: 도시별 구독 방식으로 변경

--- src/main/java/site/weather/api/weather/controller/WeatherController.java
+++ src/main/java/site/weather/api/weather/controller/WeatherController.java
@@ -1,15 +1,18 @@
 package site.weather.api.weather.controller;
 
-import java.time.Duration;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
+import site.weather.api.weather.dto.response.WeatherResponse;
 import site.weather.api.weather.service.WeatherService;
 
 @Controller
@@ -21,12 +24,31 @@ public class WeatherController {
 
 	private final SimpMessagingTemplate messagingTemplate;
 
+	private final Set<String> subscribers = ConcurrentHashMap.newKeySet();
+
+	private WeatherResponse cachedWeatherResponse;
+
 	@MessageMapping("/weather")
 	public void subscribeWeather(String city) {
-		Flux.interval(Duration.ofSeconds(5))
-			.flatMap(tick -> service.fetchWeatherByCity(city))
-			.take(Duration.ofSeconds(30))
+		subscribers.add(city);
+
+		if (cachedWeatherResponse == null) {
+			fetchWeatherByCity(city);
+		}
+	}
+
+	private void fetchWeatherByCity(String city) {
+		service.fetchWeatherByCity(city)
 			.publishOn(Schedulers.boundedElastic())
-			.subscribe(response -> messagingTemplate.convertAndSend("/topic/weather", response));
+			.log()
+			.subscribe(response -> {
+				cachedWeatherResponse = response;
+				messagingTemplate.convertAndSend("/topic/weather/" + city, response);
+			});
+	}
+
+	@Scheduled(fixedRate = 30, timeUnit = TimeUnit.SECONDS)
+	public void fetchAndBroadcastByCity() {
+		subscribers.forEach(this::fetchWeatherByCity);
 	}
 }
