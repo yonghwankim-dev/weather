0ae16d600072bf979dd1133d851dad6b6f96e869
yonghwankim-dev <dragonbead95@naver.com>
2024-11-04 14:49:52 +0900

    feat: 세션 연결/해제 이벤트 추가
    세션을 연결시 맵에 구독한 도시의 세션ID 추가
    
    세션 해제시 구독한 도시의 세션 ID 삭제

--- src/main/java/site/weather/api/weather/controller/WeatherController.java
+++ src/main/java/site/weather/api/weather/controller/WeatherController.java
@@ -1,14 +1,19 @@
 package site.weather.api.weather.controller;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,8 +31,10 @@ public class WeatherController {
 	private final SimpMessagingTemplate messagingTemplate;
 
 	private final Set<String> subscribers = ConcurrentHashMap.newKeySet();
+	// 도시별 세션 ID 컬렉션
+	private final Map<String, Set<String>> subscribersSessionId = new ConcurrentHashMap<>();
 	private final Map<String, WeatherResponse> cachedWeatherResponses = new ConcurrentHashMap<>();
-	
+
 	@MessageMapping("/weather")
 	public void subscribeWeather(String city) {
 		subscribers.add(city);
@@ -52,5 +59,39 @@ public class WeatherController {
 	@Scheduled(fixedRate = 30, timeUnit = TimeUnit.SECONDS)
 	public void fetchAndBroadcastByCity() {
 		subscribers.forEach(this::fetchWeatherByCity);
+	}
+
+	@EventListener
+	public void handleStompConnectedHandler(SessionSubscribeEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String sessionId = headerAccessor.getSessionId();
+		String destination = headerAccessor.getDestination();
+		if (destination == null) {
+			return;
+		}
+		String[] parts = destination.split("/");
+		String city = parts[parts.length - 1];
+		// subscribersSessionId에 도시가 없는 경우 새로운 HashSet을 추가
+		subscribersSessionId.computeIfAbsent(city, k -> ConcurrentHashMap.newKeySet()).add(sessionId);
+		log.info("subscribersSessionId: {}", subscribersSessionId);
+	}
+
+	@EventListener
+	public void handleStompDisconnectedHandler(SessionDisconnectEvent event) {
+		String sessionId = event.getSessionId();
+		Set<String> delCity = new HashSet<>();
+		// 도시별로 sessionId를 제거하고 제거한 Set이 비어있으면 삭제 집합에 추가한다
+		subscribersSessionId.keySet().forEach(city -> {
+			Set<String> sessionIdSet = subscribersSessionId.getOrDefault(city, new HashSet<>());
+			sessionIdSet.remove(sessionId);
+			if (sessionIdSet.isEmpty()) {
+				delCity.add(city);
+			}
+		});
+		delCity.forEach(city -> {
+			subscribersSessionId.remove(city);
+			subscribers.remove(city);
+			log.info("remove the city " + city);
+		});
 	}
 }
