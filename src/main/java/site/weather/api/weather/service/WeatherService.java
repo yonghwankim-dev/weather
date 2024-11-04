0be16edc4c58ccb0791d1a0ef00de0f56c119a46
yonghwankim-dev <dragonbead95@naver.com>
2024-11-04 16:11:42 +0900

    refactor: Service로 메서드 분리

--- src/main/java/site/weather/api/weather/service/WeatherService.java
+++ src/main/java/site/weather/api/weather/service/WeatherService.java
@@ -1,18 +1,33 @@
 package site.weather.api.weather.service;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import site.weather.api.weather.dto.response.WeatherResponse;
+import site.weather.api.weather.repository.WeatherSubscriptionInfoRepository;
 
 @Service
 @RequiredArgsConstructor
 public class WeatherService {
 
 	private final WeatherWebClient client;
+	private final WeatherSubscriptionInfoRepository repository;
+	private final SimpMessagingTemplate messagingTemplate;
 
 	public Mono<WeatherResponse> fetchWeatherByCity(String city) {
 		return client.fetchWeatherByCity(city);
+	}
+
+	public void subscribeWeatherByCity(String city) {
+		client.fetchWeatherByCity(city)
+			.publishOn(Schedulers.boundedElastic())
+			.log()
+			.subscribe(response -> {
+				repository.changeWeatherResponse(city, response);
+				messagingTemplate.convertAndSend("/topic/weather/" + city, response);
+			});
 	}
 }
