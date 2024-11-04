a47dc3614259c483cbb0a182d4203340b2e30f12
yonghwankim-dev <dragonbead95@naver.com>
2024-11-04 12:21:34 +0900

    refactor: 5초 간격으로 30초 동안 온도를 푸시하도록 수정

--- src/main/java/site/weather/api/weather/controller/WeatherController.java
+++ src/main/java/site/weather/api/weather/controller/WeatherController.java
@@ -1,23 +1,32 @@
 package site.weather.api.weather.controller;
 
+import java.time.Duration;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Mono;
-import site.weather.api.weather.dto.response.WeatherResponse;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 import site.weather.api.weather.service.WeatherService;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class WeatherController {
 
 	private final WeatherService service;
 
+	private final SimpMessagingTemplate messagingTemplate;
+
 	@MessageMapping("/weather")
-	@SendTo("/topic/weather")
-	public Mono<WeatherResponse> subscribeWeather(String city) {
-		return service.fetchWeatherByCity(city);
+	public void subscribeWeather(String city) {
+		Flux.interval(Duration.ofSeconds(5))
+			.flatMap(tick -> service.fetchWeatherByCity(city))
+			.take(Duration.ofSeconds(30))
+			.publishOn(Schedulers.boundedElastic())
+			.subscribe(response -> messagingTemplate.convertAndSend("/topic/weather", response));
 	}
 }
