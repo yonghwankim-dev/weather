75fc60fd8bcd6c155ffa1ecb45756926a45f7de7
yonghwankim-dev <dragonbead95@naver.com>
2024-11-04 11:36:56 +0900

    refactor: 불필요 getter, 생성자 제거

--- src/main/java/site/weather/api/weather/dto/response/WeatherResponse.java
+++ src/main/java/site/weather/api/weather/dto/response/WeatherResponse.java
@@ -3,17 +3,13 @@ package site.weather.api.weather.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode
-@NoArgsConstructor
-@Getter
 public class WeatherResponse {
 	@JsonProperty("name")
-	private String name;
+	private final String name;
 	@JsonProperty("temperature")
-	private double temperature;
+	private final double temperature;
 
 	public WeatherResponse(String name, double temperature) {
 		this.name = name;
