3484a3dc02fe18bb10614bad1cc032879131ca6a
yonghwankim-dev <dragonbead95@naver.com>
2024-11-04 16:11:42 +0900

    refactor: Service로 메서드 분리

--- src/main/java/site/weather/api/weather/controller/WeatherController.java
+++ src/main/java/site/weather/api/weather/controller/WeatherController.java
@@ -1,7 +1,5 @@
 package site.weather.api.weather.controller;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -11,7 +9,6 @@ import org.springframework.stereotype.Controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.scheduler.Schedulers;
 import site.weather.api.weather.domain.WeatherSubscriptionInfo;
 import site.weather.api.weather.repository.WeatherSubscriptionInfoRepository;
 import site.weather.api.weather.service.WeatherService;
@@ -22,31 +19,17 @@ import site.weather.api.weather.service.WeatherService;
 public class WeatherController {
 
 	private final WeatherService service;
-
 	private final SimpMessagingTemplate messagingTemplate;
-
-	// 도시별 구독 정보
-	private final Map<String, WeatherSubscriptionInfo> weatherSubscriptionInfoMap = new ConcurrentHashMap<>();
 	private final WeatherSubscriptionInfoRepository repository;
 
 	@MessageMapping("/weather")
 	public void subscribeWeather(String city) {
 		WeatherSubscriptionInfo weatherSubscriptionInfo = repository.subscribeWeather(city);
-		weatherSubscriptionInfo.sendOrFetchWeather(messagingTemplate, city, this::fetchWeatherByCity);
-	}
-
-	private void fetchWeatherByCity(String city) {
-		service.fetchWeatherByCity(city)
-			.publishOn(Schedulers.boundedElastic())
-			.log()
-			.subscribe(response -> {
-				repository.changeWeatherResponse(city, response);
-				messagingTemplate.convertAndSend("/topic/weather/" + city, response);
-			});
+		weatherSubscriptionInfo.sendOrFetchWeather(messagingTemplate, city, service::subscribeWeatherByCity);
 	}
 
 	@Scheduled(fixedRate = 30, timeUnit = TimeUnit.SECONDS)
 	public void fetchAndBroadcastByCity() {
-		repository.findAllCities().forEach(this::fetchWeatherByCity);
+		repository.findAllCities().forEach(service::subscribeWeatherByCity);
 	}
 }
