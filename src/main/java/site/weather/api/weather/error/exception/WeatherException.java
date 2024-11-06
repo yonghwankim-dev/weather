f21318b627f7f23c30fa69e26008932f6d8b5522
yonghwankim-dev <dragonbead95@naver.com>
2024-11-06 16:22:33 +0900

    feat: WeatherException에 httpStatusCode 필드 추가
    - 404인 경우 잘못된 도시 이름으로 판단하여 저장소에서 제거하기 위함

--- src/main/java/site/weather/api/weather/error/exception/WeatherException.java
+++ src/main/java/site/weather/api/weather/error/exception/WeatherException.java
@@ -1,15 +1,24 @@
 package site.weather.api.weather.error.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
 public class WeatherException extends RuntimeException {
+	private final HttpStatusCode httpStatusCode;
 	private final String message;
 
-	public WeatherException(String message) {
+	public WeatherException(HttpStatusCode httpStatusCode, String message) {
 		super(message);
+		this.httpStatusCode = httpStatusCode;
 		this.message = message;
+	}
+
+	public boolean is404Error() {
+		return this.httpStatusCode == HttpStatus.NOT_FOUND;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("(message=%s)", message);
+		return String.format("(httpStatusCode=%s, message=%s)", httpStatusCode, message);
 	}
 }
